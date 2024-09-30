125976925d78910d4707d1c66b1ed4d42c8800a0
wennerryle <wennerryle@gmail.com>
2024-09-30 22:46:59 +0300

    update schedule

--- src/ui/molecules/LessonCardTypes.d.ts
+++
@@ -1,7 +0,0 @@
-export default class LessonCard {
-  public startTime: string;
-  public endTime: string;
-  public lessonName: string;
-  public cabinet: number | string;
-  public note: string;
-}
